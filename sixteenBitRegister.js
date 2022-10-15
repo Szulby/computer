47ffe323547e1c4fb74d90fd1b14d81a7f15a58e
Slawomir <s.sulbinski@sprzeglo.com.pl>
2022-10-15 10:50:00 +0200

    16bitRegister

---
+++ sixteenBitRegister.js
@@ -0,0 +1,17 @@
+import oneBitRegister from './oneBitRegister.js';
+
+export default function sixteenBitRegister() {
+	const state = Array(16)
+		.fill(oneBitRegister)
+		.map((bit) => bit());
+	return function (input, load) {
+		return state.map((dff, id) => dff(input[id], load));
+	};
+}
+
+// const register = sixteenBitRegister();
+
+// console.log(register([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1));
+// console.log(register([0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1));
+// console.log(register([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0));
+// console.log(register([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0));
