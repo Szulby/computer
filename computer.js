614283a500ae076b27198b1de4ed37f567a1f07b
Slawomir <s.sulbinski@sprzeglo.com.pl>
2022-10-15 10:12:31 +0200

    dff improvment

--- computer.js
+++ computer.js
@@ -1,16 +1,11 @@
-import alu from './alu.js'
-import dff from './dff.js'
+import alu from './alu.js';
+import dff from './dff.js';
+import mux from './mux.js';
 
-let counter = 10
-while (counter) {
-	let rand = Math.round(Math.random(0, 1))
-	console.log(rand, dff(rand))
-	counter--
-}
-// console.log(
-// 	alu(
-// 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-// 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-// 		[1, 1, 0, 0, 0, 1]
-// 	)
-// )
+console.log(
+	alu(
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+		[1, 1, 0, 0, 0, 1]
+	)
+);
--- dff.js
+++ dff.js
@@ -1,7 +1,9 @@
-let dffStore
-
-export default function dff(i) {
-	let tmp = dffStore
-	dffStore = i
-	return tmp
+export default function dff() {
+	let state = 0;
+	function next(input) {
+		let tmp = state;
+		state = input;
+		return tmp;
+	}
+	return next;
 }
