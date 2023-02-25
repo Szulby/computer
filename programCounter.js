bffea0ad001fd3689c26ef94aba397fa7b1e1af1
Slawomir <s.sulbinski@sprzeglo.com.pl>
2023-02-25 20:50:02 +0100

    fix pc

--- programCounter.js
+++ programCounter.js
@@ -1,24 +1,21 @@
-import sixteenBitRegister from './sixteenBitRegister.js';
-import inc16 from './inc16.js';
+import sixteenBitRegister from "./sixteenBitRegister.js";
+import inc16 from "./inc16.js";
 
 export default function programCounter() {
-	const register = sixteenBitRegister();
-	return function (inp, load, inc, reset) {
-		if (reset) {
-			return register([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1);
-		}
-		if (load) {
-			return register(inp, load);
-		}
-		if (inc) {
-			return register(inc16(inp), 1);
-		}
-		return register(inp, load);
-	};
+  const register = sixteenBitRegister();
+  return function (inp = [], load, reset) {
+    if (reset) {
+      return register([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1);
+    }
+    if (load) {
+      return register(inp, load);
+    }
+    return register(inc16(register()), 1);
+  };
 }
 
-const pc = programCounter();
+// const pc = programCounter();
 
-console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 1, 0, 0));
-console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 0, 1, 0));
-console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 0, 1, 0));
+// console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 1, 0));
+// console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 1, 1));
+// console.log(pc([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1], 1, 1));
