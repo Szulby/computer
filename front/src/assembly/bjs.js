800f54dc960b009e6a192f13fd3e925eeb5bc2ed
Slawomir <s.sulbinski@sprzeglo.com.pl>
2023-08-07 23:33:14 +0200

    import function system

--- front/src/assembly/bjs.js
+++ front/src/assembly/bjs.js
@@ -1,6 +1,26 @@
 import fs from "fs";
+const infinite = `
+function infinite
+  label end
+  goto end
+return
+`;
+const add = `
+function add 
+  push argument 0
+  push argument 1
+  add
+return
+`;
+const functions = {
+  infinite,
+  add,
+};
 
 const input = `
+import infinite
+import add
+
 push constant 2
 push constant 3
 push constant 4
@@ -9,32 +29,27 @@ call add 2
 
 call infinite 0
 
-function add 
-  push argument 0
-  push argument 1
-  add
-return
-
-function infinite
-  label end
-  goto end
-return
-
-
 
 // eq
 // if-goto
 // sub
 `;
 const output = [];
+const functionsDefs = [];
 input
   .split("\n")
   .filter((el) => el && !el.includes("//"))
   .map((el) => el.trim())
   .forEach((el) => {
-    output.push(el);
+    if (el.includes("import")) {
+      functionsDefs.push(el.split(" ")[1]);
+    } else {
+      output.push(el);
+    }
   });
-
+functionsDefs.forEach((def) => {
+  output.push(functions[def]);
+});
 fs.writeFile("./vm.txt", output.join("\n"), (err) => {
   if (err) console.log(err);
   else console.log("written correctly");
